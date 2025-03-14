cc0618bb5f91f11ccc828f7374ffea76d274c067
Prosper Ibe <Prosperibe12@gmail.com>
2025-03-14 21:13:14 +0000

    fixed db in clusters

--- project_core/settings/production.py
+++ project_core/settings/production.py
@@ -19,9 +19,9 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'postgres-service',
         'PORT': '5432',
     }
